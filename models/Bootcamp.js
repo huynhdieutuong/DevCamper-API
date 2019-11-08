94242146eace0bd5c345e8185dbd5b1c7eb79d6d
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-11-08 16:47:27 +0700

    Bootcamp basic CRUD

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -40,12 +40,12 @@ const BootcampSchema = new mongoose.Schema({
     // GeoJSON Point
     type: {
       type: String,
-      enum: ['Point'],
-      required: true
+      // required: true,
+      enum: ['Point']
     },
     coordinates: {
       type: [Number],
-      required: true,
+      // required: true,
       index: '2dsphere'
     },
     formattedAddress: String,
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const bootcamps = require('./routes/bootcamps');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Body parser
+app.use(express.json());
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));

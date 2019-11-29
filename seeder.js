700448530937c36149bd1101820cc5d4cc55329d
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-11-29 19:40:16 +0700

    Bootcamp & User Relationship

--- seeder.js
+++ seeder.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,12 +23,16 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
 
 // Import data
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-    // await Course.create(courses);
+    await Course.create(courses);
+    await User.create(users);
     console.log('Data Imported...'.green.inverse);
     process.exit();
   } catch (error) {
@@ -40,6 +45,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log('Data Destroyed...'.red.inverse);
     process.exit();
   } catch (error) {

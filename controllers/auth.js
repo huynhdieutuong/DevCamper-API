776fb6d936a8ce9b78f53af688b588ce595f73b6
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-12-01 15:12:18 +0700

    Password reset token generation

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,6 +45,41 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc    Get logged in user
+// @route   GET /api/v1/auth/me
+// @access  Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: req.user
+  });
+});
+
+// @desc    Forgot password
+// @route   POST /api/v1/auth/forgotpassword
+// @access  Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(
+      new ErrorResponse(
+        `There is no user with the email address ${req.body.email}`,
+        404
+      )
+    );
+  }
+
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
@@ -69,13 +104,3 @@ const sendTokenResponse = (user, statusCode, res) => {
       token
     });
 };
-
-// @desc    Get logged in user
-// @route   GET /api/v1/auth/me
-// @access  Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    data: req.user
-  });
-});
--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,19 @@ const router = express.Router();
 
 const { protect } = require('../middlewares/auth');
 
-const { register, login, getMe } = require('../controllers/auth');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword
+} = require('../controllers/auth');
 
 router.route('/register').post(register);
 
 router.route('/login').post(login);
 
 router.route('/me').get(protect, getMe);
+
+router.route('/forgotpassword').post(forgotPassword);
 
 module.exports = router;
